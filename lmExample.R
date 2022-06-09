f04382c7af1e3ebf3f20e45e3961d1f5601a81da
Max <u1380468@umail.edu>
2022-06-09 16:28:21 +0900

    Added a comment for each section.

--- lmExample.R
+++ lmExample.R
@@ -1,3 +1,4 @@
+#Now let’s add an R script. In RStudio, click [File] > [New File] > [R Script] to open a blank R script. Add the following R code to this script:
 x = rnorm(100, mean = 20, sd = 5)
 y = x + rnorm(100, mean = 0, sd = 2)
 
@@ -5,5 +6,7 @@ fit = lm(y ~ x)
 
 summary(fit)
 
-plot(x,y)
+#Let’s add a couple more changes. First, we’ll add code to plot the data, and the model fit to the end of the file:
+plo
+t(x,y)
 abline(fit, col=2)
